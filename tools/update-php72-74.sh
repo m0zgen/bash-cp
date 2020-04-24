8e41df46eb4cb06dc2250805441539fbea335bb5
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2020-04-24 14:34:24 +0300

    update 72 to 74 fixed

--- tools/update-php72-74.sh
+++ tools/update-php72-74.sh
@@ -2,5 +2,7 @@
 
 rpm -e $(rpm -qa | grep php)
 yum module reset php
-yum module install php:remi-7.4
-yum install -y php-json php-xml php-pdo php-pear php-xmlrpc php-gd php-cli php-fpm php-pspell php-pecl-memcache php-snmp php-process php-mysqlnd php php74-php-xml php-pecl-apcu php-pecl-redis
+yum -y module install php:remi-7.4
+yum -y install php-json php-xml php-pdo php-pear php-xmlrpc php-gd php-cli php-fpm php-pspell php-pecl-memcache php-snmp php-process php-mysqlnd php php74-php-xml php-pecl-apcu php-pecl-redis
+
+systemctl start php-fpm && systemctl enable php-fpm
